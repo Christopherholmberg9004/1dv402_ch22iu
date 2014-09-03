8d8a40969d47f521961c34aeec79bfbebd6d2032
1dv402_ch22iu <christopherholmberg@live.se>
2014-09-03 16:19:54 +0200

    Made all classes.

--- exchange_money_v37/exchange_money_v37/Program.cs
+++ exchange_money_v37/exchange_money_v37/Program.cs
@@ -6,22 +6,30 @@ using System.Threading.Tasks;
 
 namespace exchange_money_v37
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(");
+        }
+        private static double ReadPositiveDouble(string prompt = null)
+        {
+            Console.WriteLine("")
+        return();
+        }
+        private static uint ReadUint(string prompt, uint minValue)
+        {
+          return(50);
+        }
+        private static uint[] SplitIntoDenominations(uint changw, uint[] denominations)
+        {
+            return(denominations);
+        }
+        private static void ViewMessage(string message, [bool isError = false])
+        private static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint cash, uint change, uint[] notes, uint[] denominations);
+        private static void roundingOffAmount()
+        {
             
         }
-        static void ReadPositiveDouble(string prompt)
-        {
-        
-        }
-        static void ReadUint(string prompt, unit minValue)
-        {
-
-        }
-        static void SplitIntoDenominations(uint minValue);
-
-        static void ViewMessage(string message, [bool isError = false]);
     }
 }
