f8c8077dba9b38ae9348e95886fda9eef52eb246
Christopherholmberg9004 <christopherholmberg@live.se>
2014-09-03 14:15:48 +0200

    My first app

---
+++ exchange_money_v37/exchange_money_v37/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exchange_money_v37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first app!");
+            Console.ReadLine();
+        }
+    }
+}
