9bccfa3c0a30cc2a096ae8e6423b2d4484c92adb
1dv402_ch22iu <christopherholmberg@live.se>
2014-09-03 16:45:36 +0200

    Read,TryParse, while-loop

--- exchange_money_v37/exchange_money_v37/Program.cs
+++ exchange_money_v37/exchange_money_v37/Program.cs
@@ -10,12 +10,47 @@ namespace exchange_money_v37
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(");
+            Console.WriteLine(ReadPositiveDouble("ReadPositiveDouble test, amount: "));
         }
         private static double ReadPositiveDouble(string prompt = null)
         {
-            Console.WriteLine("")
-        return();
+            double startValue = 0;
+
+            bool startReading = true;
+            
+            while (startReading)
+            {
+                {
+                    // Skriver ut..
+                    if (prompt != null)
+                    {
+                        Console.Write(prompt);
+                    }
+
+                    startValue = 0;
+                    // Kollar upp och byter ut värde.
+                    if (Double.TryParse(Console.ReadLine().Replace('.', ','), out startValue))
+                    {
+                        double fixValue = Math.Round(startValue, MidpointRounding.AwayFromZero);
+                        if (fixValue > 0)
+                        {
+                            startReading = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error! '{0}' to small value.", startValue);
+                            Console.ReadLine();
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! Error!");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            return startValue;
         }
         private static uint ReadUint(string prompt, uint minValue)
         {
@@ -25,11 +60,8 @@ namespace exchange_money_v37
         {
             return(denominations);
         }
-        private static void ViewMessage(string message, [bool isError = false])
-        private static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint cash, uint change, uint[] notes, uint[] denominations);
-        private static void roundingOffAmount()
-        {
-            
-        }
+        // private static void ViewMessage(string message);
+       // private static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint cash, uint change, uint[] notes, uint[] denominations);
+        // private static void roundingOffAmount()
     }
 }
